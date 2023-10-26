0c58f3a2ab9bc9ef0c10897ee35bdc61209a66a6
Ivan Milincic <kreativan.dev@gmail.com>
2023-10-26 12:56:19 +0300

    init upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TextformatterExternalLinks
+
 ProcessWire Textformatter module that will add `_blank` and `nofollow` attributes to all external links in the content.
+
+## How to use
+1. Copy the module files into directory /site/modules/TextformatterExternalLinks/
+2. Click “Modules > Refresh” in ProcessWire Admin. Click install for the module labeled: “Textformatter External Links”.
+3. In any Textarea field using CKEditor or TinyMCE select `Textformatter External Links` for the Text formaters option.
---
+++ TextformatterExternalLinks.module.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * TextformatterExternalLinks
+ * @author Ivan Milincic <hello@kreativan.dev>
+ * @link http://www.kraetivan.dev
+ */
+
+namespace ProcessWire;
+
+class TextformatterExternalLinks extends Textformatter implements ConfigurableModule {
+
+  public static function getModuleInfo() {
+    return array(
+      'title' => 'Textformatter External Links',
+      'version' => 100,
+      'summary' => 'Add "_blank" and "nofollow" attributes to all external links',
+      'author' => 'Ivan Milincic',
+      'href' => '',
+    );
+  }
+
+  public function __construct() {
+    parent::__construct();
+  }
+
+  public function format(&$value) {
+
+    $links = $this->find_external_links($value);
+    if (!$links) return $value;
+
+    $replace = "<a target='_blank' rel='nofollow noopener noreferrer'";
+    foreach ($links as $link) {
+      $value = str_replace("<a", $replace, $value);
+    }
+  }
+
+  public function find_external_links($value) {
+    // find all external links using regex in $value
+    $pattern = '/<a\s+(?![^>]*\btarget=["\']_blank["\'])[^>]*\bhref=["\'](https?:\/\/[^"\']+)["\'][^>]*>/i';
+    preg_match_all($pattern, $value, $matches);
+    if (empty($matches[0])) return;
+    $links = [];
+    foreach ($matches as $match) {
+      // ifmatch starts with <a
+      if (substr($match[0], 0, 2) == "<a") {
+        $links[] = $match[0];
+      }
+    }
+    return $links;
+  }
+}
